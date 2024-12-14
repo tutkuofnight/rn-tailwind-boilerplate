d5aa911365292683c4d60cdb787995b2d707332c
Tutku Ucan <mettheus0@gmail.com>
2024-12-15 02:44:22 +0300

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-white bg-black p-2 rounded-md text-center">React Native Tailwind Boilerplate</Text>
+    </View>
+  );
+}
